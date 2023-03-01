5b41821e6e4eca9222f1b1b23ba3b9a588f8b71b
zerktea <kerere1999@gmail.com>
2023-03-01 13:56:22 +0100

    Sign in form

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import Form from './components/Form'
 import './index.css'
 function App() {
-  
-
+ 
   return (
     <div className="App">
       <Form/>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,48 @@
 import React from "react"
 import '../index.css'
 import { FaUserAlt } from 'react-icons/fa';
-import { FaEye } from 'react-icons/fa';
 import EyePass from "./Eyepass";
 export default function Form(){
     const [isPass,setPass]=React.useState("password")
+    const [form,setForm] =
+    React.useState({
+      name:"",
+      surname:"",
+      email:"",
+      gender:"",
+      password:"",
+      confirmpassword:"",
+      newletter: false
+    })
+   
+    function handleForm(event){
+       event.target.type==="checkbox"? setForm({
+        ...form,
+        newletter: !form.newletter
+    }):
+       setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+        
+    }
     function handleClick(){
        isPass==="password"?setPass("text"):setPass("password")
     }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        if(form.password === form.confirmpassword){
+            if(form.gender === ''){
+                alert("ARE YOU A TREE, Please identify your gender")
+                return
+            }
+            alert("Wlecom to the jungle " + form.name)
+            return
+        }
+        alert("Passwords doesnt match")
+    }
+
     return(
         <div className="form--div">
             <div className="form--header">
@@ -17,24 +52,71 @@ export default function Form(){
             
             <h4>Please Enter your details so sign in</h4>
             </div>
-        <form action="" className="form">
+        <form action="" className="form" onSubmit={handleSubmit}>
             {/* <label htmlFor="name"  >Name</label> */}
-            <input type="text" id="name" placeholder="Name"/>
+            <input 
+                type="text"
+                name="name" 
+                placeholder="Name" 
+                value={form.name} 
+                onChange={handleForm}
+            />
             
-            <input type="text" id="surname" placeholder="Surname"/>
-            <input type="email" id="email" placeholder="Email"/>
+            <input 
+                type="text" 
+                name="surname"
+                placeholder="Surname"
+                value={form.surname} 
+                onChange={handleForm}
+            />
+
+            <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                placeholder="Email"
+                value={form.email} 
+                onChange={handleForm}
+            />
+
             <div className="pass">
-                <input type={isPass} id="password" placeholder="Password"/>
+                <input 
+                    type={isPass} 
+                    name="password" 
+                    id="password" 
+                    placeholder="Password" 
+                    value={form.password} 
+                    onChange={handleForm}
+                />
                 <EyePass
                     value={isPass}
                     handleClick={handleClick}
                 /> 
             </div>
-            <input type="password" id="re-password" placeholder="Repeat password"/>
+
+            <input 
+                type="password"
+                name="confirmpassword" 
+                placeholder="Repeat password" 
+                value={form.confirmpassword} 
+                onChange={handleForm}
+            />
+            <select name="gender" value={form.gender} onChange={handleForm}>
+                <option value="">Grapefruit</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+            </select>
             <div className="checkbox">
-                <input type="checkbox" name="newspaper" id="newspaper" />
+                <input 
+                    type="checkbox" 
+                    name="newletter"
+                    onChange={handleForm} 
+                    checked={form.newletter} 
+                />
+                
                 <label htmlFor="newspaper"> Sub to our Newsletter</label>
             </div>
+
             <button className="form--submit">Sign In</button>
         </form>
         </div>
